e94d2b4ac9d000850aa9305a8d662e2764ee750f
Dasol Kim <50112481+SoulfulSol@users.noreply.github.com>
2020-12-01 00:39:29 +0900

    Update q_2178_Sol.py
    Shortened

--- Graph/q_2178_Sol.py
+++ Graph/q_2178_Sol.py
@@ -1,3 +1,27 @@
+# from collections import deque
+
+# if __name__ == '__main__':
+#     N, M = map(int, input().split(" "))
+#     arr = [list(input()) for i in range(N)]
+#     arr[0][0] = '-1'
+#     dis = 1
+#     que = deque([dis, 0, 0])
+#     dir = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+#     while que:
+#         x, y, dis = que.pop(), que.pop(), que.pop()
+#         if x == N - 1 and y == M - 1:
+#             res = dis
+#             break
+#         for i in dir:
+#             nextX = x + i[0]
+#             nextY = y + i[1]
+#             if 0 <= nextX < N and 0 <= y + i[1] < M and arr[nextX][nextY] == '1':
+#                 arr[nextX][nextY] = '-1'
+#                 que.appendleft(nextX)
+#                 que.appendleft(nextY)
+#                 que.appendleft(dis + 1)
+#     print(dis)
+
 from collections import deque
 
 if __name__ == '__main__':
@@ -5,10 +29,10 @@ if __name__ == '__main__':
     arr = [list(input()) for i in range(N)]
     arr[0][0] = '-1'
     dis = 1
-    que = deque([dis, 0, 0])
+    que = deque([[0, 0, dis]])
     dir = [[0, 1], [1, 0], [-1, 0], [0, -1]]
     while que:
-        x, y, dis = que.pop(), que.pop(), que.pop()
+        x, y, dis = que.popleft()
         if x == N - 1 and y == M - 1:
             res = dis
             break
@@ -17,7 +41,5 @@ if __name__ == '__main__':
             nextY = y + i[1]
             if 0 <= nextX < N and 0 <= y + i[1] < M and arr[nextX][nextY] == '1':
                 arr[nextX][nextY] = '-1'
-                que.appendleft(nextX)
-                que.appendleft(nextY)
-                que.appendleft(dis + 1)
+                que.append([nextX, nextY, dis + 1])
     print(dis)
