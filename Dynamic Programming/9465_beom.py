3354193f4a6db6dd26bf9ab358910c8a36df1ca5
kibeom lee <70303094+2012160085@users.noreply.github.com>
2020-12-04 17:48:20 +0900

    Create 9465_beom.py

---
+++ Dynamic Programming/9465_beom.py
@@ -0,0 +1,15 @@
+
+case_num = int(input())
+for i in range(case_num):
+
+    sticker_num = int(input())
+    up = list(map(int, input().split()))
+
+    down = list(map(int, input().split()))
+
+    U,D, N = [up[0]], [down[0]],[0]
+    for i in range(1, sticker_num):
+        U.append(up[i] + max(D[i-1], N[i-1]))
+        D.append(down[i]+ max(U[i-1],N[i-1]))
+        N.append(max(U[i-1],D[i-1], N[i-1]))
+    print(max(U[-1],   D[-1], N[-1]))
