3f22f263384ce6c0758a21a17a0657e87dfc437e
kibeom lee <70303094+2012160085@users.noreply.github.com>
2020-12-06 19:51:35 +0900

    Update and rename 퇴사_못품_.py to 퇴사_beom.py

--- Code Review Problems(12.06)/퇴사_beom.py
+++ Code Review Problems(12.06)/퇴사_beom.py
@@ -6,7 +6,7 @@ def price(i):
         if tp[i][0] + i <= days:
             return max(tp[i][1]+price(i+tp[i][0]),price(i+1))
         else:
-            return 0
+            return price(i+1)
     else:
         return 0
 print(price(0))
