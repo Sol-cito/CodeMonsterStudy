96d5f1476f7972dc7966187938d5dec5fc84faf0
dataenggu <dsolkim1030@gmail.com>
2020-12-09 01:37:27 +0900

    Solve 7569

--- Graph/q_7569_sol.py
+++ Graph/q_7569_sol.py
@@ -1,9 +1,32 @@
 import sys
+from _collections import deque
 
-# 1. for문 한바퀴 돌면서 안익은 토마토(0) 개수를 구하고, queue에 토마토(1) 좌표를 담음 -> [x좌표, y좌표, z좌표, 일수]
-# 2. queue를 bfs하면서 토마토를 익히고, 안익은 토마토 개수에서 익힌 토마토 개수를 뺌
-# 3. 안익은 토마토 개수가 0이 되면 그 일수를 반환함.
-# 4. while이 끝났는데도 안익은 토마토 개수가 1개가 넘으면 -1 반환, 아니면 일수 반환
+def bfs(to, arr, que, M, N, H):
+    directions = [[0, 0, 1], [0, 1, 0], [0, -1, 0], [0, 0, -1], [1, 0, 0], [-1, 0, 0]]
+    while que:
+        pop = que.pop()
+        for dir in directions:
+            nh, nr, nc = pop[0] + dir[0], pop[1] + dir[1], pop[2] + dir[2]
+            if 0 <= nh < H and 0 <= nr < N and 0 <= nc < M and arr[nh][nr][nc] == 0:
+                arr[nh][nr][nc] = -1
+                que.appendleft([nh, nr, nc, pop[3] + 1])
+                to -= 1
+                if to == 0: return pop[3] + 1
+    return -1
+
 
 M, N, H = map(int, sys.stdin.readline().rstrip().split(" "))
 arr = [[list(map(int, sys.stdin.readline().split(" "))) for i in range(N)] for i in range(H)]
+to = 0
+que = deque()
+for i in range(M):
+    for j in range(N):
+        for k in range(H):
+            if arr[k][j][i] == 0:
+                to += 1
+            elif arr[k][j][i] == 1:
+                que.appendleft([k, j, i, 0])
+if to == 0:
+    print(0)
+else:
+    print(bfs(to, arr, que, M, N, H))
