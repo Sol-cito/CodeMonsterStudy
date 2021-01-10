a81bf7de6bda0fee50a1a178333c23db45d127d3
b0ho <ybg4828@gmail.com>
2021-01-10 21:58:29 +0900

    5427

--- HOME/src/Main.java
+++ HOME/src/Main.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class Main {
 
     static int T;
-    static int A;
-    static int B;
-    static boolean[] visit;
-    static int tmp;
-    static String answer;
+    static int w;
+    static int h;
+    static char[][] arr;
+    static int[][] fire;
+    static Queue<Fire> fire_q;
+    static boolean[][] visit;
+    static int[][] drx;
+    static Person start;
+    static int answer;
 
     public static void main(String[] args) {
 
@@ -16,68 +20,151 @@ public class Main {
 
         while (T-- > 0) {
 
-            A = sc.nextInt();
-            B = sc.nextInt();
-            visit = new boolean[10000];
+            h = sc.nextInt();
+            w = sc.nextInt();
+            arr = new char[w][h];
+            fire = new int[w][h];
+            fire_q = new LinkedList<>();
+            visit = new boolean[w][h];
+            drx = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
-            bfs();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    fire[i][j] = w * h;
+                }
+            }
 
-            System.out.println(answer);
+            for (int i = 0; i < w; i++) {
+                String str = sc.next();
+                arr[i] = str.toCharArray();
+
+                for (int j = 0; j < h; j++) {
+                    if (arr[i][j] == '@') {
+                        start = new Person(i, j, 0);
+                    }
+                    if (arr[i][j] == '*') {
+                        fire_q.add(new Fire(i, j));
+                        fire[i][j] = 0;
+                    }
+                }
+
+            }
+
+            //print(arr);
+
+            fire();
+
+            move();
+
+            if (answer == -1) {
+                System.out.println("IMPOSSIBLE");
+            } else {
+                System.out.println(answer);
+            }
+
         }
+
     }
 
+    public static void fire() {
 
-    public static void bfs() {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(A, ""));
-        visit[A] = true;
+        while (!fire_q.isEmpty()) {
+
+            for (int i = 0; i < fire_q.size(); i++) {
+                Fire p = fire_q.poll();
+                int x = p.a;
+                int y = p.b;
+
+                for (int[] ints : drx) {
+                    int nx = x + ints[0];
+                    int ny = y + ints[1];
+
+                    if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
+                        if (arr[nx][ny] != '#') {
+                            if (fire[nx][ny] > fire[x][y] + 1) {
+                                fire[nx][ny] = fire[x][y] + 1;
+                                fire_q.add(new Fire(nx, ny));
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void move() {
+        Queue<Person> q = new LinkedList<>();
+        q.add(new Person(start.a, start.b, start.c));
+        visit[start.a][start.b] = true;
 
         while (!q.isEmpty()) {
-            Point p = q.poll();
-            int num = p.a;
-            String func = p.b;
+            Person p = q.poll();
+            int x = p.a;
+            int y = p.b;
+            int cnt = p.c;
 
-            if (num == B) {
-                answer = func;
+            if (x == 0 || y == 0 || x == w - 1 || y == h - 1) {
+                answer = cnt + 1;
+                return;
             }
 
-            tmp = num * 2 % 10000;
-            if (!visit[tmp]) {
-                visit[tmp] = true;
-                q.add(new Point(tmp, func + "D"));
-            }
+            for (int[] ints : drx) {
+                int nx = x + ints[0];
+                int ny = y + ints[1];
 
-            tmp = num - 1 < 0 ? 9999 : num - 1;
-            if (!visit[tmp]) {
-                visit[tmp] = true;
-                q.add(new Point(tmp, func + "S"));
-            }
+                if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
+                    if (arr[nx][ny] == '.') {
+                        if (!visit[nx][ny]) {
+                            if (fire[nx][ny] > cnt + 1) {
+                                visit[nx][ny] = true;
+                                q.add(new Person(nx, ny, cnt + 1));
+                            }
+                        }
+                    }
+                }
 
-            tmp = num % 1000 * 10 + num / 1000;
-            if (!visit[tmp]) {
-                visit[tmp] = true;
-                q.add(new Point(tmp, func + "L"));
-            }
-
-            tmp = num % 10 * 1000 + num / 10;
-            if (!visit[tmp]) {
-                visit[tmp] = true;
-                q.add(new Point(tmp, func + "R"));
             }
 
         }
 
+        answer = -1;
+
     }
 
-    public static class Point {
+    public static class Fire {
         int a;
-        String b;
+        int b;
 
-        public Point(int a, String b) {
+        public Fire(int a, int b) {
             this.a = a;
             this.b = b;
         }
+    }
 
+    public static class Person {
+        int a;
+        int b;
+        int c;
+
+        public Person(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+    }
+
+    public static void print(char[][] array) {
+        for (int z = 0; z < w; z++) {
+            for (int y = 0; y < h; y++) {
+                System.out.print(array[z][y]);
+            }
+            System.out.println();
+        }
     }
 
 }
