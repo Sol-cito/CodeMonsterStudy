735485528609ec69f5a9204dea372e625ee05758
b0ho <ybg4828@gmail.com>
2021-01-31 19:07:32 +0900

    17135

--- HOME/src/Main.java
+++ HOME/src/Main.java
@@ -5,8 +5,13 @@ public class Main {
     static int N;
     static int M;
     static int D;
-    static int G;
     static int[][] arr;
+    static int[][] map;
+    static ArrayList<Integer> archer;
+    static boolean[][] visit;
+    static int dist;
+    static int mx;
+    static int my;
     static int answer;
 
     public static void main(String[] args) throws IOException {
@@ -15,9 +20,10 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         D = Integer.parseInt(st.nextToken());
-        G = 3;
+        arr = new int[N][M];
+        map = new int[N][M];
+        archer = new ArrayList<>();
 
-        arr = new int[N + 1][];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -25,27 +31,104 @@ public class Main {
             }
         }
 
-        // 3 궁수의 자리를 정한다
-        choice();
+        comb(0, 3);
 
         System.out.println(answer);
 
     }
 
-    public static void choice() {
+    // 3 궁수의 조합을 구한다.
+    public static void comb(int start, int r) {
+        if (r == 0) {
+            attack();
+            return;
+        }
 
+        for (int i = start; i < M; i++) {
+            archer.add(i);
+            comb(i + 1, r - 1);
+            archer.remove(archer.size() - 1);
+        }
     }
 
+    // 궁수가 공격하고 적을 제거한다.
     public static void attack() {
+        init();
+        int sum = 0;
 
+        for (int i = 0; i < N; i++) {
+            visit = new boolean[N][M];
+            for (Integer arc : archer) {
+                mx = Integer.MAX_VALUE;
+                my = Integer.MAX_VALUE;
+                dist = Integer.MAX_VALUE;
+
+                choice(arc);
+
+                if (dist <= D) {
+                    visit[mx][my] = true;
+                }
+            }
+
+            sum = check(sum);
+            move();
+
+        }
+
+        answer = Math.max(answer, sum);
     }
 
+    // 맵 초기화
+    public static void init() {
+        for (int i = 0; i < N; i++) {
+            if (M >= 0) System.arraycopy(arr[i], 0, map[i], 0, M);
+        }
+    }
+
+    // 공격할 적을 선택한다. 바로 죽이지는 않고,
+    public static void choice(int pos) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                    if (dist > getDist(i, j, N, pos)) {
+                        dist = getDist(i, j, N, pos);
+                        mx = i;
+                        my = j;
+                    } else if (dist == getDist(i, j, N, pos) && my > j) {
+                        mx = i;
+                        my = j;
+                    }
+                }
+            }
+        }
+    }
+
+    // 거리 구하는 공식을 적용한다.
+    public static int getDist(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    }
+
+    // 적을 죽이고, 점수를 계산한다.
+    public static int check(int sum) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (visit[i][j]) {
+                    sum++;
+                    map[i][j] = 0;
+                }
+            }
+        }
+        return sum;
+    }
+
+    // 남은 적들이 한칸씩 내려온다
     public static void move() {
-
-    }
-
-    public static void check() {
-
+        for (int i = N - 1; i > 0; i--) {
+            if (M >= 0) System.arraycopy(map[i - 1], 0, map[i], 0, M);
+        }
+        for (int i = 0; i < M; i++) {
+            map[0][i] = 0;
+        }
     }
 
 }
