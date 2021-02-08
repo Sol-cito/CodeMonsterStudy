f535f56ddfa4cf6a1d7345e02f15e92856b22551
b0ho <ybg4828@gmail.com>
2021-02-08 23:28:15 +0900

    trie

--- HOME/src/Main.java
+++ HOME/src/Main.java
@@ -2,63 +2,93 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int L;
-    static int W;
-    static int H;
     static int N;
-    static int[][] arr;
-    static boolean flag;
+    static int M;
+    static String[] S;
+    static String[] arr;
+    static Trie root;
     static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][2];
+        S = new String[N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = (int) Math.pow(2, Integer.parseInt(st.nextToken()));
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            S[i] = st.nextToken();
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            public int compare(int[] o1, int[] o2) {
-                return o2[0] - o1[0];
+        arr = new String[M];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = st.nextToken();
+        }
+
+        root = new Trie();
+
+        for (int i = 0; i < N; i++) {
+            insert(S[i]);
+        }
+
+        answer = 0;
+
+        for (int i = 0; i < M; i++) {
+            if (find(arr[i])) {
+                answer++;
             }
-        });
-
-        divide(L, W, H, 0);
-
-        if (flag) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);
         }
+
+        System.out.println(answer - 1);
+
     }
 
-    public static void divide(int l, int w, int h, int idx) {
-        if (l == 0 || w == 0 || h == 0) {
-            return;
-        }
-        for (int i = idx; i < arr.length; i++) {
-            if (arr[i][1] != 0 && l >= arr[i][0] && w >= arr[i][0] && h >= arr[i][0]) {
-                arr[i][1]--;
-                answer++;
-                divide(l - arr[i][0], w, h, i);
-                divide(arr[i][0], w - arr[i][0], h, i);
-                divide(arr[i][0], arr[i][0], h - arr[i][0], i);
-                return;
+    public static void insert(String word) {
+        Trie Node = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char alpha = word.charAt(i);
+            int idx = alpha - 'a';
+
+            if (Node.node[idx] == null) {
+                Trie tmp = new Trie();
+                Node.node[idx] = tmp;
+                Node = tmp;
+            } else {
+                Node = Node.node[idx];
             }
         }
 
-        flag = true;
+        Node.finish = true;
+    }
 
+    public static boolean find(String word) {
+        Trie Node = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char alpha = word.charAt(i);
+            int idx = alpha - 'a';
+
+            if (Node.node[idx] == null) {
+                return false;
+            } else {
+                Node = Node.node[idx];
+            }
+        }
+
+        return true;
+    }
+
+    public static class Trie {
+        Trie[] node;
+        boolean finish;
+
+        public Trie() {
+            this.node = new Trie[26];
+            this.finish = false;
+        }
     }
 
 }
