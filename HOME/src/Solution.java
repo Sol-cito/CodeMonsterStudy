3f467dfd2d74bf57e2934cdf539527b310ad1dc5
b0ho <ybg4828@gmail.com>
2021-04-21 21:35:42 +0900

    ~

--- HOME/src/Solution.java
+++ HOME/src/Solution.java
@@ -1,48 +1,45 @@
 import java.util.*;
 
 class Solution {
-    int M;
-    int N;
-    int[][] dp;
-    int answer;
 
-    public static void main(String[] args) {
+    public static void main(String arsg[]) {
         Solution s = new Solution();
-        int m = 4;
-        int n = 3;
-        int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
-
-        s.solution(m, n, computers);
+        int[] a = {93, 30, 55};
+        int[] b = {1, 30, 5};
+        System.out.println(s.solution(a, b));
     }
 
-    public int solution(int m, int n, int[][] puddles) {
-        M = m;
-        N = n;
-        dp = new int[M + 1][N + 1];
-        answer = 0;
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList();
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] answer;
 
-        for (int[] puddle : puddles) {
-            dp[puddle[0]][puddle[1]] = -1;
-        }
-
-        dp[1][1] = 1;
-
-        for (int i = 1; i < M + 1; i++) {
-            for (int j = 1; j < N + 1; j++) {
-                if (dp[i][j] == -1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if (i != 1) {
-                    dp[i][j] += dp[i - 1][j] % 1000000007;
-                }
-                if (j != 1) {
-                    dp[i][j] += dp[i][j - 1] % 1000000007;
-                }
+        for (int i = 0; i < progresses.length; i++) {
+            int num;
+            if ((100 - progresses[i]) % speeds[i] == 0) {
+                num = (100 - progresses[i]) / speeds[i];
+            } else {
+                num = (100 - progresses[i]) / speeds[i] + 1;
             }
+            queue.add(num);
         }
 
-        answer = (int) dp[m][n] % 1000000007;
+        while (!queue.isEmpty()) {
+            int cnt = 1;
+            int now = queue.poll();
+            while (!queue.isEmpty() && now >= queue.peek()) {
+                queue.poll();
+                cnt++;
+            }
+            list.add(cnt);
+        }
+
+        answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
         return answer;
     }
 }
